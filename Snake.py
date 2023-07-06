0d7b9bd98d4481af5c40cb1f3497a5cc96433269
Will Simpson <60453692+TheBossMan237@users.noreply.github.com>
2023-07-06 12:51:06 -0700

    Update Snake.py

--- Snake.py
+++ Snake.py
@@ -1,21 +1,22 @@
 import pygame as py
 from random import randint
+from Classes import Text
+
 py.init()
 from time import sleep
-win = py.display.set_mode((500, 500))
 EV_FRUIT_EATEN = py.USEREVENT+1
-
 class Snake:
-    def __init__(self):
+    def __init__(self, win : py.Surface):
         self.S = [[250, 250], [225, 250]]
         self.Score = 0
         self.GameOver = False
         self.Fruit = [[randint(0, 20)*25, randint(0, 20)*25] for i in range(10)]
+        self.win = win
     def Draw(self):
         for x in self.S:
-            py.draw.rect(win, (0, 255, 0), (x[0], x[1], 25, 25))
+            py.draw.rect(self.win, (0, 255, 0), (x[0], x[1], 25, 25))
         for x in self.Fruit:
-            py.draw.rect(win, (255, 0, 0), (x[0], x[1], 25, 25))
+            py.draw.rect(self.win, (255, 0, 0), (x[0], x[1], 25, 25))
     def Move(self, Dir : int):
         OnFruit = False
         for x in range(0, len(self.Fruit)):
@@ -56,9 +57,8 @@ class Snake:
 
 
 
-py.time.set_timer(py.USEREVENT, 100)
-S = Snake()
-def Main():
+
+def Main(win : py.Surface):
 
     Dir = 0
     X, Y = 25, 25
@@ -67,7 +67,7 @@ def Main():
         win.fill((0,0,0))
         for ev in py.event.get():
             if ev.type == py.QUIT:
-                return -1
+                return -2
             if ev.type == py.MOUSEMOTION:
                 mx, my = ev.pos
             if ev.type == py.KEYDOWN:
@@ -79,6 +79,8 @@ def Main():
                     Dir = 2
                 if ev.key == py.K_a and Dir != 1:
                     Dir = 3
+                if ev.key == py.K_q: 
+                    return -1
             if ev.type == py.USEREVENT:
                 if S.GameOver:
                     return 1
@@ -88,33 +90,30 @@ def Main():
                 pass
         S.Draw()
         py.display.flip()
-def Game_Over():
-    Font = py.font.Font(None, 32)
-    Texts = [
-        Font.render("Game Over!", True, (255, 255, 255)),
-        Font.render(f'Score: {S}', True, (255, 255, 255), ),
-        Font.render("Press Space To Try Again", True, (255, 255, 255), )
-    ]
+def Game_Over(win : py.Surface):
+    Text.DefualtSize(50)
+    Texts = [Text("Game Over!", (0,0)), Text(f"Score - {S}", (0, 30)), Text("Space - Try Again", (0,60)), Text("q - To Main Menu", (0, 90))]
+    for x in Texts:
+        win.blit(*x.Rend)
     S.Draw()
-    win.blit(Texts[0], (0, 0))
-    win.blit(Texts[1], (0, 30))
-    win.blit(Texts[2], (0, 50))
     py.display.flip()
     while True:
 
         for ev in py.event.get():
             if ev.type == py.QUIT:
-                return -1
-            if ev.type == py.KEYDOWN and ev.key == py.K_SPACE:
-                S.Reset()
-                return 1
-
-if __name__ == "__main__":
-    State = 1
+                return -2
+            if ev.type == py.KEYDOWN:
+                if ev.key == py.K_SPACE:
+                    S.Reset()
+                    return 1
+                else:
+                    return -2
+def Play_Snake(win : py.Surface):
+    py.time.set_timer(py.USEREVENT, 100)
+    global S
+    S = Snake(win)
     while True:
-        State = Main()
-        if(State == -1): break
-        State = Game_Over()
-        if(State == -1): break
-    py.quit()
-    exit()
+        State = Main(win)
+        if(State < 0): return State
+        State = Game_Over(win)
+        if(State < 0): return State
