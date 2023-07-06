c69c2f8cba8a95fd7d406c18b3c74a36b2b4a0ee
Will Simpson <60453692+TheBossMan237@users.noreply.github.com>
2023-07-06 12:52:07 -0700

    Add files via upload

---
+++ Classes.py
@@ -0,0 +1,29 @@
+from typing import overload, Tuple
+from pygame.font import Font
+from pygame.sprite import Sprite
+from pygame import Surface, Rect, Vector2
+class Text:
+    S = 0
+    @staticmethod
+    def DefualtSize(val : int):
+        Text.S = val
+    @overload
+    def __init__(self,Size : int, _Text : str,Pos = (0,0), Color = (255, 255, 255)):
+        self.T = Font(None, Size).render(_Text, True, Color)
+        self._Text = _Text
+        self.P = Pos
+        self.R = Rect(Pos, self.T.get_size())
+    def __init__(self, _Text : str, Pos = (0,0), Color = (255, 255, 255)):
+        self.T = Font(None, Text.S).render(_Text, True, Color)
+        self._Text = _Text
+        self.P = Pos
+        self.R = Rect(Pos, self.T.get_size())
+    def Touching(self, Other : Tuple[int,int]): 
+        if(self.R.collidepoint(Other)):
+            return self._Text
+        else:
+            return None
+    @property
+    def Rend(self):
+        return (self.T, self.P)
+    
---
+++ Main.py
@@ -0,0 +1,47 @@
+import pygame as py
+win = py.display.set_mode((500, 500))
+from Pong import Play_Pong
+from Snake import Play_Snake
+from Classes import Text
+
+if __name__ == "__main__":
+    Text.DefualtSize(50)
+    Texts = [Text("Select Game", (0,0)),Text("Play Snake", (0, 30)), Text("Play Pong", (0, 60))]
+    Selected = None
+    Games = {
+        "Play Snake" : Play_Snake,
+        "Play Pong" : Play_Pong 
+    }
+    for x in Texts:
+        win.blit(*x.Rend)
+    py.display.update()
+    while True:
+        for ev in py.event.get():
+            if ev.type == py.QUIT:
+                py.quit()
+                exit()
+            if ev.type == py.MOUSEBUTTONDOWN:
+                for x in Texts[1:]:
+                    S = x.Touching(py.mouse.get_pos())
+                    if(S != None):
+                        Selected = S
+                        break
+                    
+                    Selected = None
+        if(Selected != None):
+            while True:
+                State = Games[Selected](win)
+                if(State == -1): break
+                if(State == -2): 
+                    py.quit()
+                    exit()
+            win.fill((0,0,0))
+            for x in Texts:
+                win.blit(*x.Rend)
+            py.display.update()
+            Selected = None
+
+
+                    
+    py.quit()
+    exit()
