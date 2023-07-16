69f07a5a58048d290a11409aba238e3c7cdb9d88
Will Simpson <60453692+TheBossMan237@users.noreply.github.com>
2023-07-16 17:15:41 -0400

    Add files via upload

--- Node/Minecraft_Piano/Main.js
+++ Node/Minecraft_Piano/Main.js
@@ -1,17 +1,19 @@
 const websocket = require("ws");
 const wss = new websocket.Server({port : 8080});
-let Computers = new Map(); /**CC:Tweaked Connections */
+/**Website */let Client = null ;
+/**CC:Twekaed Connections */let Computers = new Map();
 function print(msg) {
     console.log(msg)
 }
 wss.on("connection", (ws, msg) => {
-    /** recive the message */
     ws.on("message", (data) => {
         if(ws.protocol == "") {
             Computers.set(data.toString(), ws)
             ws.send("Done")
+            print(Computers)
         } else {
             try {
+                print(data.toString())
                 Computers.get(data.toString()).send("Start")
             } catch (error) {
                 
