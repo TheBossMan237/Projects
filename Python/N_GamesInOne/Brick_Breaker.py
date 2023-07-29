eab091016eb4de6758d73ffda12302c1a41adab5
Will Simpson <60453692+TheBossMan237@users.noreply.github.com>
2023-07-29 09:18:30 -0700

    Update Brick_Breaker.py

--- Python/N_GamesInOne/Brick_Breaker.py
+++ Python/N_GamesInOne/Brick_Breaker.py
@@ -13,25 +13,35 @@ class Blocks:
     List = []
     Width = 50
     Height = 25
+    Destroyed = 0
     for x in range(50, 401, Width):
         for y in range(50, 201, Height):
             List.append([py.Rect(x, y, Width, Height), True])
 
 class Ball:
-    Speed = 2
-    Radius = 10
-    Rect = py.Rect(250-Radius, 250-Radius, 2*Speed*Radius, 2*Speed*Radius)
-    CX, CY, = 250, 250
-    Angle = random()*pi
-    Vel = py.Vector2(Speed*cos(Angle), Speed*sin(Angle))
+    def __init__(self):
+
+        Ball.Speed = 2
+        Ball.Radius = 10
+        Ball.Rect = py.Rect(250-Ball.Radius, 250-Ball.Radius, 2*Ball.Speed*Ball.Radius, 2*Ball.Speed*Ball.Radius)
+        Ball.CX, Ball.CY, = 250, 250
+        Ball.Angle = random()*pi/2
+        Ball.TouchedRect = False
+        Ball.Vel = py.Vector2(Ball.Speed*cos(Ball.Angle + pi/2), Ball.Speed*sin(Ball.Angle + pi/2))
     
     GameOver = False
     @classmethod 
     def Move(cls):
         if cls.CX > 500-cls.Radius:
             cls.Vel.reflect_ip(py.Vector2(1, 0))
+            cls.Bounced_X = False
+            cls.Bounced_Y = False
+            print("Right")
         if cls.CX < 0+cls.Radius:
             cls.Vel.reflect_ip(py.Vector2(-1, 0))
+            cls.Bounced_X = False
+            cls.Bounced_Y = False
+            print("Left")
         if cls.CY > 500-cls.Radius-Paddle.Height:
             if cls.CY > 500-cls.Radius:
                 cls.GameOver = True
@@ -39,10 +49,17 @@ class Ball:
                 X = (cls.Rect.centerx - Paddle.Rect.centerx)*2 / Paddle.Width
                 cls.Vel = py.Vector2(0, -cls.Speed)
                 cls.Vel.rotate_rad_ip(-(pi/2)*X)
+                cls.Bounced_X = False
+                cls.Bounced_Y = False
 
                 
         if cls.CY < 0+cls.Radius:
             cls.Vel.reflect_ip(py.Vector2(0, -1))
+            cls.Bounced_X = False
+            cls.Bounced_Y = True
+            print("Top")
+
+        
         cls.CX += cls.Vel.x
         cls.CY += cls.Vel.y
         cls.Rect.center = (cls.CX, cls.CY)
@@ -51,19 +68,18 @@ class Ball:
         return (Ball.CX, Ball.CY)
     @classmethod
     def Bounce(cls, Rect : py.Rect):
-        
-        if cls.Vel.y > 0:
+        if cls.Vel.x >= 0 and not cls.Bounced_X:
             cls.Vel.reflect_ip(py.Vector2(0, 1))
-            print("Top")
-        if cls.Vel.y < 0:
+            cls.Bounced_X = True
+            return 
+        if cls.Vel.x <= 0 and not cls.Bounced_X:
             cls.Vel.reflect_ip(py.Vector2(0, -1))
-            print("Bottom")
-        if cls.Vel.x > 0 :
+            cls.Bounced_X = True
+            return
+        if cls.Vel.y >= 0 and not cls.Bounced_Y:
             cls.Vel.reflect_ip(py.Vector2(1, 0))
-            print("Right")
-        if cls.Vel.x < 0:
-            cls.Vel.reflect_ip(py.Vector2(-1, 0))
-            print("Left")
+            cls.Bounced_Y = True
+        
 
     @CLS_Prop
     def S_Rect(cls):
@@ -72,8 +88,11 @@ class Ball:
         return X
 
 def Main(win : py.Surface):
+    Ball()
     win = py.display.set_mode((500, 500))
-    Time = py.time.Clock()    
+    Time = py.time.Clock()
+    win.fill((0,0,0)) 
+    py.display.flip()   
     while True:
         for ev in py.event.get():
             if ev.type == py.QUIT:
@@ -104,13 +123,23 @@ def Main(win : py.Surface):
                     py.draw.rect(win, (0,0,0), x[0])
                     x[1] = False
                     py.display.update(x[0])
+                    Blocks.Destroyed += 1
+                    if(Blocks.Destroyed == len(Blocks.List)):
+                        Ball.Won = True;
+                        return 1
                     continue
                 py.display.update(x[0])
         Time.tick(120)
                 
                 
 def Main_Menu(win : py.Surface):
-
+    win.fill((0,0,0))
+    Ball.GameOver = False
+    Ball.Won = False
+    Texts = [Text("Play Again?", (0, 50))]
+    for x in Texts:
+        win.blit(*x.CenteredX)
+    py.display.flip()
     while True:
         for ev in py.event.get():
             if ev.type == py.QUIT:
@@ -118,8 +147,12 @@ def Main_Menu(win : py.Surface):
             if ev.type == py.KEYDOWN:
                 if ev.key == py.K_q:
                     return -1
-                if ev.key == py.K_SPACE:
+            if ev.type == py.MOUSEBUTTONDOWN:
+                if Texts[0].Touching(py.mouse.get_pos()) == "Play Again?":
+                    print(1)
                     return 0
+
+
                 
 
 
@@ -130,5 +163,3 @@ def Play_Brick_Breaker(win):
         if(State < 0): return State
         State = Main(win)
         if(State < 0): return State
-
-py.quit()
