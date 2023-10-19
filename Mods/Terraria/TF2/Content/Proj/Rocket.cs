13ba62bab34f26de06a9bebae6c486bdb9d5a521
Will Simpson <60453692+TheBossMan237@users.noreply.github.com>
2023-10-18 21:09:26 -0400

    Update Rocket.cs

--- Mods/Terraria/TF2/Content/Proj/Rocket.cs
+++ Mods/Terraria/TF2/Content/Proj/Rocket.cs
@@ -1,20 +1,24 @@
-﻿using Terraria.ID;
+using Terraria.ID;
 using Terraria;
 using Terraria.ModLoader;
-
+using TF2.Assets;
+using Terraria.Audio;
+using System;
+using Terraria.DataStructures;
 namespace TF2.Content.Proj
 {
-    internal class Rocket : ModProjectile
+    public class Rocket : ModProjectile
     {
+        
+
         public override void SetDefaults()
         {
-            Projectile.width = 10; // The width of projectile hitbox
-            Projectile.height = 10; // The height of projectile hitbox
-            Projectile.aiStyle = ProjAIStyleID.Beam;
+            Projectile.width = 10; 
+            Projectile.height = 10; 
             Projectile.friendly = true;
             Projectile.hostile = false;
             Projectile.penetrate = 100;
-            Projectile.timeLeft = 30;
+            Projectile.timeLeft = 300;
             Projectile.light = 0f;
             Projectile.ignoreWater = false;
             Projectile.tileCollide = true;
@@ -27,10 +31,19 @@ namespace TF2.Content.Proj
         }
         public override void OnKill(int timeLeft)
         {
+
+
+
+            //Rocket does 50% damage when at range;
             Player player = Main.player[Projectile.owner];
-            if (Projectile.position.Distance(player.position) < 78.1225) {
+            
+            SoundEngine.PlaySound(Sounds.rocket_explode);
+            float distance = Projectile.position.Distance(player.position);
+            if (distance < 78.1225) {
+                distance = 78.1225f - distance;
                 Projectile.oldVelocity = Projectile.oldVelocity * -1;
-                player.velocity += Projectile.oldVelocity;
+                player.velocity += Projectile.oldVelocity * (distance / 15.6245f);
+                player.statLife--;
             }
 
             base.OnKill(timeLeft);
